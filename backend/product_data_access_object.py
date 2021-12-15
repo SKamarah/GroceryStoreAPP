656bd9fa9eee2ebf9f90581876bb603f109dff06
SKamarah <k98.saleem@gmail.com>
2021-12-14 19:05:37 -0500

    added server code

--- backend/product_data_access_object.py
+++ backend/product_data_access_object.py
@@ -1,10 +1,13 @@
 from sql_connection import get_sql_connection
 
-def get_All_Products(connection):
+
+def get_all_products(connection):
     cursor = connection.cursor()
 
-    query = (" SELECT Products.product_id, Products.Name, Products.unit_id, Products.price_per_unit, unit_table.unit_measure_name "
-             "FROM Products inner join unit_table on Products.unit_id = unit_table.unit_id")
+    query = (
+        " SELECT Products.product_id, Products.Name, Products.unit_id, Products.price_per_unit, "
+        "unit_table.unit_measure_name "
+        "FROM Products inner join unit_table on Products.unit_id = unit_table.unit_id")
 
     cursor.execute(query)
 
@@ -23,7 +26,8 @@ def get_All_Products(connection):
 
     return response
 
-def insert_new_product(connection,product):
+
+def insert_new_product(connection, product):
     cursor = connection.cursor()
     query = ("INSERT INTO products "
              "(name, unit_id, price_per_unit)"
@@ -44,9 +48,8 @@ def delete_product(connection, product_id):
 
     return cursor.lastrowid
 
-if __name__ ==  '__main__':
+
+if __name__ == '__main__':
     connection = get_sql_connection()
-    #print(get_All_Products(connection))
-    print(delete_product(connection,7))
-
-
+    #print(get_all_products(connection))
+    print(delete_product(connection, 7))
--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -2,10 +2,11 @@ import mysql.connector
 
 __cnx = None
 
+
 def get_sql_connection():
     global __cnx
     if __cnx is None:
         __cnx = mysql.connector.connect(user='root', password='S4xt0n18',
-                                  host='127.0.0.1',
-                                  database='Shopping_Cart')
+                                        host='127.0.0.1',
+                                        database='Shopping_Cart')
     return __cnx
